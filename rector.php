9af281ea6c878db196b320a1e6a2286414764588
zingimmick <zingimmick@outlook.com>
2022-10-29 23:08:42 +0800

    Bump minimum version of PHP to 8.0

--- rector.php
+++ rector.php
@@ -13,7 +13,7 @@ use Rector\Set\ValueObject\LevelSetList;
 use Zing\CodingStandard\Set\RectorSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([LevelSetList::UP_TO_PHP_73, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_80, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
     $rectorConfig->parallel();
     $rectorConfig->skip(
         [
